84f63956ca52a1c1c163fe769dd8b004da4e94d8
meashishpatel <ashishpatel3009@gmail.com>
2024-05-01 09:54:53 +0530

    Implement buffer overflow attack simulation

--- All.c
+++ All.c
@@ -429,3 +429,43 @@ int main ()
 
 // Secret key for the Alice is: 9
 // Secret Key for the Bob is: 9
+
+
+
+// Exp: 14 Aim: Implement a code to simulate buffer overflow attack.
+
+// Hardware / Software Required : Stack Guard compiler.
+
+// 	Theory:
+// A buffer overflow, or buffer overrun, is an anomaly where a program, while writing data to a buffer, overruns the buffer's boundary and overwrites adjacent memory locations. This is a special case of the violation of memory safety.
+// A buffer overflow occurs when a program or process tries to store more data in a buffer (temporary data storage area) than it was intended to hold.
+// Buffer overflow: Code:
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char buff[15];
+    int pass = 0;
+
+    printf("\n Enter the password : \n");
+    gets(buff);
+
+    if(strcmp(buff, "thecorrectpaswd"))
+    {
+        printf ("\n Wrong Password \n");
+    }
+    else
+    {
+        printf ("\n Correct Password \n");
+        pass = 1;
+    }
+
+    if(pass)
+    {
+        /* Now Give root or admin rights to user */
+        printf ("\n Root privileges given to the user \n");
+    }
+
+    return 0;
+}
